53875ddea15bfcc97a4c1bec5cde0b12127b27ba
kaio15922 <kaiorenato12345@gmail.com>
2026-06-28 17:53:21 +0000

    arrumando saida dos fbwrite

--- kmain.c
+++ kmain.c
@@ -2,17 +2,20 @@
 #include "serial.h"
 
 void kmain() {
-    // 1. Inicializa o hardware da porta série silenciosamente
+    // Inicializa o hardware da porta serial
+    // Imprime um log de info
     serial_configure(SERIAL_COM1_BASE);
     log_message(LOG_INFO, "Sistema Iniciado! Porta Serie configurada.");
 
-    // 2. Cria uma mensagem e testa a lógica de escrita do ecrã
-    char texto[] = "Vai tomar no cu JF\n";
-    char texto2[] = "David eh gente boa\n";
+    // Cria uma mensagem e testa a lógica de escrita do ecrã
+    char texto[] = "Printando na tela com sucesso.\n";
+    char texto2[] = "Gears of War melhor que God of War.\n";
+    char texto3[] = "Cap 4 feito!\n";
     // Enviamos a string inteira, e a função cuidará do loop e do \n sozinha
     fb_write(texto, sizeof(texto) - 1);
     fb_write(texto2, sizeof(texto2) - 1);
+    fb_write(texto3, sizeof(texto3) - 1);
 
-    // 3. Imprime um log final confirmando o sucesso
+    // Imprime um log (debug) final confirmando o sucesso
     log_message(LOG_DEBUG, "O kernel concluiu a execucao com sucesso.");
 }
